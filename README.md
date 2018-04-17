75cf7f73c1988002ca084b42594748f96b882e50
Amy Keith <amyakeith@gmail.com>
2018-04-16 20:44:20 -0500

    Add all files

---
+++ README.md
@@ -0,0 +1,1 @@
+I'm making this etch-a-sketch tool using HTML, CSS, Javascript, and JQuery as part of The Odin Project curriculum.
---
+++ etch.js
@@ -0,0 +1,10 @@
+/*
+var str = prompt("We're going to make an Etch-A-Sketch!\nYou get to determine how detailed our Etch-A-Sketch will be by choosing it's size.\nPlease enter a number greater than or equal to 16 (the higher the better!).");
+var x = Number(str);
+var y = x;
+*/
+
+$(document).ready(function() {
+$("#box").append("<div id='square'></div>");
+});
+*/
