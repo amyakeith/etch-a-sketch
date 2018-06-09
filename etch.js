19e288b404a77e944d53c36392d2a8af5057a0f6
Amy Keith <amyakeith@gmail.com>
2018-06-09 07:43:18 -0500

    Make erase button functional: grid will prompt user to enter new size, grid will clear

--- etch.js
+++ etch.js
@@ -1,16 +1,24 @@
 // get the number of divs from the user
-var str = prompt("We're going to make an Etch-A-Sketch!\nYou get to determine how detailed our Etch-A-Sketch will be.\nPlease enter a multiple of 4.");
-var x = Number(str);
+function getNumItems()
+{
+  var str = prompt("We're going to make an Etch-A-Sketch!\nYou get to determine how detailed our Etch-A-Sketch will be.\nPlease enter a multiple of 4.");
+  var x = Number(str);
+  return x;
+}
 
-// set the height and width of the boxes that will make up the div
-var gridItemHeight = 480 / x;
-var gridItemWidth = 480 / x;
+// set the measurement for the height and width of the boxes that will make up the div
+function getItemSize(n)
+{
+  var size = 480 / n;
+  return size;
+}
 
-function fillGrid(numItems, itemHeight, itemWidth)
+
+function fillGrid(num, itemHeight, itemWidth)
 {
   // create an array of divs
   const grid = [];
-  for (var counter = 0; counter <= Math.pow(numItems, 2); counter++)
+  for (var counter = 1; counter <= Math.pow(num, 2); counter++)
   {
     grid[counter] = document.createElement('div');
     grid[counter].classList.add("smallDiv");
@@ -18,26 +26,41 @@ function fillGrid(numItems, itemHeight, itemWidth)
 
   // populate the box with an x by y sized grid
   const box = document.querySelector("#box");
-  for (var counter1 = 0; counter1 <= Math.pow(numItems, 2); counter1++)
+  for (var counter1 = 1; counter1 <= Math.pow(num, 2); counter1++)
   {
     grid[counter1].style.display = "inline-block";
-    //grid[counter1].style.marginBottom = -4 + "px";
     grid[counter1].style.height = itemHeight + "px";
     grid[counter1].style.width = itemWidth + "px";
     box.appendChild(grid[counter1]);
   }
 
-  const smallDivs = document.querySelectorAll(".smallDiv");
-  smallDivs.forEach((div) => {
+  const gridBox = document.querySelectorAll(".smallDiv");
+  gridBox.forEach((div) => {
   div.addEventListener('mouseenter', function(e){
     e.target.style.background = "black";
   });
 });
-
 }
 
-fillGrid(x, gridItemHeight, gridItemWidth);
+// when clear button is clicked, erase the current grid and prompt
+//user to ask
+function clearGrid()
+{
+  const bigBox = document.querySelector("#box");
+  while (bigBox.firstChild) {
+  bigBox.removeChild(bigBox.firstChild);
+  }
+}
+
+var numBoxes = getNumItems();
+var gridItemSize = getItemSize(numBoxes);
+fillGrid(numBoxes, gridItemSize, gridItemSize);
 
 
-
-// when clear button is clicked, erase the current grid and prompt user to ask
+var btn = document.querySelector('#button');
+btn.addEventListener('click', function(){
+  clearGrid();
+  var numBoxes = getNumItems();
+  var gridItemSize = getItemSize(numBoxes);
+  fillGrid(numBoxes, gridItemSize, gridItemSize);
+});
